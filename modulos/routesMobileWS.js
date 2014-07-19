a849d4289fda2b382adb543fd839bbd4f97de5ab
fabiolenine <fabiolenine@icloud.com>
2014-07-18 21:56:23 -0300

    Correção
    Correção do rota.

--- modulos/routesMobileWS.js
+++ modulos/routesMobileWS.js
@@ -5,7 +5,7 @@ module.exports = function(app, passport) {
     // set up Express routes to handle incoming requests
     //
     // roteamento do serviço web
-    appMobileWS.get('/api/', function(req,res)
+    app.get('/api', function(req,res)
         {
             res.send('Olá Sam Bell, estou aqui para lhe ajudar!');
         });
