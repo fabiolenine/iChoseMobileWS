cc28e6393cdb011271b447ac808cd2828f0ad1e5
fabiolenine <fabiolenine@icloud.com>
2014-07-19 13:01:11 -0300

    Criptografia
    Retirei a criptografia.

--- modulos/routesProvider.js
+++ modulos/routesProvider.js
@@ -18,7 +18,7 @@ module.exports = function(app, passport) {
     // process the login form
 	app.post('/login', passport.authenticate('local-login', {
 		successRedirect   : '/dashboard', // redirect to the secure profile section
-		failureRedirect   : '/',          // redirect back to the signup page if there is an error
+		failureRedirect   : '/accessdenied',          // redirect back to the signup page if there is an error
 		failureFlash      : true          // allow flash messages
 	}));
 
