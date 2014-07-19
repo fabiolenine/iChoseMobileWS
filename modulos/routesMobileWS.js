22479a616c61944d30e9fb23409cd2acadaac68f
fabiolenine <fabiolenine@icloud.com>
2014-07-18 21:54:34 -0300

    Rotas
    Criação do arquivo routesMobileWS.js, para as rotas do appMobileWS.

---
+++ modulos/routesMobileWS.js
@@ -0,0 +1,13 @@
+// app/routesMobileWS.js
+module.exports = function(app, passport) {
+
+    // ------------------------------------------------------------------------
+    // set up Express routes to handle incoming requests
+    //
+    // roteamento do serviço web
+    appMobileWS.get('/api/', function(req,res)
+        {
+            res.send('Olá Sam Bell, estou aqui para lhe ajudar!');
+        });
+
+}
