1fcd955db927133a18c09730352fd73f7631d612
fabiolenine <fabiolenine@icloud.com>
2014-07-19 15:31:13 -0300

    login
    testes

--- modulos/routesProvider.js
+++ modulos/routesProvider.js
@@ -15,6 +15,12 @@ module.exports = function(app, passport) {
         res.render('index.ejs', { message: req.flash('loginMessage') }); // load the index.ejs file
     });
 
+    	app.get('/loginteste', function(req, res) {
+
+		// render the page and pass in any flash data if it exists
+		res.render('loginteste.ejs', { message: req.flash('loginMessage') }); 
+	});
+    
     // process the login form
 	app.post('/login', passport.authenticate('local-login', {
 		successRedirect   : '/dashboard',     // redirect to the secure profile section
@@ -38,16 +44,15 @@ module.exports = function(app, passport) {
 	// =====================================
 	// show the signup form
 	app.get('/signup', function(req, res) {
-
 		// render the page and pass in any flash data if it exists
 		res.render('signup.ejs', { message: req.flash('signupMessage') });
 	});
     
     // process the signup form
 	app.post('/signup', passport.authenticate('local-signup', {
-		successRedirect : '/dashboard',   // redirect to the secure profile section
-		failureRedirect : '/signup',      // redirect back to the signup page if there is an error
-		failureFlash : true               // allow flash messages
+		successRedirect   : '/dashboard', // redirect to the secure profile section
+		failureRedirect   : '/signup',    // redirect back to the signup page if there is an error
+		failureFlash      : true          // allow flash messages
 	}));
     
   // =====================================
