24ea4b1db75677ad8d57e9f6caf2242a93d20ff7
fabiolenine <fabiolenine@icloud.com>
2014-07-21 20:29:58 -0300

    HotSite
    Definição do path.

--- modulos/routesHotSite.js
+++ modulos/routesHotSite.js
@@ -1,6 +1,6 @@
 // app/routesWS.js
 module.exports = function(app, passport) {
-    var hotsitedetalhes = require('./modulos/HotSiteDetalhes.js')(configmail, mongoose, nodemailer);
+    var hotsitedetalhes = require('..ichosemobilews/modulos/HotSiteDetalhes.js')(configmail, mongoose, nodemailer);
     app.set('views', './ichosehotsite/iChoseWebSite');
 
 	// =====================================
