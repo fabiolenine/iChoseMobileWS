a32ded3c4c51f86d8bd3be3ec2970153365095f3
fabiolenine <fabiolenine@icloud.com>
2014-07-26 22:51:07 -0300

    ManagementWeb
    PassWord

--- config/passport.js
+++ config/passport.js
@@ -16,14 +16,14 @@ module.exports = function(senha)
 // passport needs ability to serialize and unserialize users out of session
 
 // used to serialize the user for the session
-  passport.serializeUser(function(user, done)
+  senha.serializeUser(function(user, done)
   {
     done(null, user.id);
   });
 
 // used to deserialize the user
 // Melhorar esse DeserializeUse área de risco de segurança
-  passport.deserializeUser(function(id, done)
+  senha.deserializeUser(function(id, done)
   {
     UserProvider.findById(id, function(err, user)
     {   
