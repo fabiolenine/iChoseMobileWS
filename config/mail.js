3879c1194ef280d4c998fe1151f264309c8bcd43
fabiolenine <fabiolenine@icloud.com>
2014-09-15 20:49:06 -0300

    Verao2015
    Teste de SES

--- config/mail.js
+++ config/mail.js
@@ -2,12 +2,14 @@ module.exports = {
                   service           : "SES",
                   host              : "email-smtp.us-west-2.amazonaws.com",
                   port              : 587,
-                  secureConnection  : false,
+                  secureConnection  : true,
                   //name: "servername",
-                  auth: {
-                          user: "AKIAJKORSFHUALWMOGOA",
-                          pass: "AgYuP3kHPUeCJWyYxVCP/ixk2PKBx4l32D5dnXR9RqJR"
-                        },
+                  //auth: {
+                  //        user: "AKIAJKORSFHUALWMOGOA",
+                  //        pass: "AgYuP3kHPUeCJWyYxVCP/ixk2PKBx4l32D5dnXR9RqJR"
+                  //      },
+                  AWSAccessKeyID    : "AKIAJKORSFHUALWMOGOA",
+                  AWSSecretKey      : "AgYuP3kHPUeCJWyYxVCP/ixk2PKBx4l32D5dnXR9RqJR", 
                   ignoreTLS         : false,
                   debug             : false,
                   maxConnections    : 5 // Default is 5
--- modulos/HotSiteDetalhes.js
+++ modulos/HotSiteDetalhes.js
@@ -11,45 +11,31 @@ module.exports = function(config, mongoose, nodemailer)
             //Registrar o novo e-mail
             //Enviar um e-mail de confirmação.
             
-			var user = emailverao2015.model.findOne(
-				{
-					'email': Email
-				},
-				function findemail(err,doc)
-				{
-					if (err)
-					{
-						//Ocorreu um erro na busca.
-						callback(false);
-					}
-					else
-					{
-						if (doc)
-						{
+			var user = emailverao2015.model.findOne({'email': Email},
+				function findemail(err,doc){   if(err) {callback(false);}
+					                           else    {
+						if(doc){
                             //acrescentar aqui o registro do e-mail no banco e envio
 							var triedverao = new triedemailverao2015.model ({ email      : Email
-                                                                             //,
-														                      // loc        : {type: 'Point', coordinates:[Lon,Lat]}
+                                                                             //,loc        : {type: 'Point', coordinates:[Lon,Lat]}
 													                         });
                             triedverao.save(function(err) { if(err) {
-                      						console.log('Erro, ' + err + ', ao tentar salvar a tentativa de envio repetido a conta: ' + Email);}
-                              					          });
+                      						console.log('Erro, ' + err + ', ao tentar salvar a tentativa de envio repetido a conta: ' + Email);}});
 							callback(false);
 						} 
-						else 
-						{   
+						else {   
                             var verao = new emailverao2015.model ({   email   : Email,
 												                //    loc     : {type: 'Point', coordinates: [Lon,Lat]},
 																      utilizou: false 
                                                                     });
                             verao.save(function(err,doc) {  if(err) {callback(false);}
-                                                            else    {var smtpTransport = nodemailer.createTransport('SMTP', config.mail);
+                                                            else    {var smtpTransport = nodemailer.createTransport('SES', config.mail);
                                                                      confirmarEmailUrl += '/?account=' + doc._id;
 						                                             smtpTransport.sendMail({from: 'verao2015@ichose.com.br',
                                                                                              to: doc.email,
 								                                                             subject: 'iChose - Verão 2015.',
 								                                                             text: 'Confirme aqui o recebimento do e-mail: ' + confirmarEmailUrl},function saveemailverao2015(err){ if(err) {callback(false);}
-								                         else    {callback(true);}});    
+								                         else    {callback(true);}                                                                                                                });    
                                                                     }
                                                          });    
 						}
