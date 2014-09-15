94b5b1cb9d13db139da250fefef3e57721109618
fabiolenine <fabiolenine@icloud.com>
2014-09-14 22:36:14 -0300

    verao2015
    Alteração de seqüência de fluxo, pois primeiro tem que registrar em
    banco para depois enviar o e-mail.

--- modulos/HotSiteDetalhes.js
+++ modulos/HotSiteDetalhes.js
@@ -38,32 +38,20 @@ module.exports = function(config, mongoose, nodemailer)
 						} 
 						else 
 						{   
-                        var smtpTransport = nodemailer.createTransport('SMTP', config.mail);
-						confirmarEmailUrl += '/?account=' + doc._id;
-						smtpTransport.sendMail(
-							{
-								from: 'verao2015@ichose.com.br',
-								to: doc.email,
-								subject: 'iChose - Verão 2015.',
-								text: 'Confirme aqui o recebimento do e-mail: ' + confirmarEmailUrl
-							},
-							function saveemailverao2015(err)
-							{
-								if (err)
-								{
-									callback(false);
-								}
-								else
-								{
-									var verao = new emailverao2015.model ({ email   : Email,
-																        //    loc     : {type: 'Point', coordinates: [Lon,Lat]},
-																            utilizou: false 
-                               												});
-                                 	verao.save(function(err) {  if(err) {callback(false);}
-                                                                else    {callback(true);}
-                                                           	 });
-								}
-							});
+                            var verao = new emailverao2015.model ({   email   : Email,
+												                //    loc     : {type: 'Point', coordinates: [Lon,Lat]},
+																      utilizou: false 
+                                                                    });
+                            verao.save(function(err,doc) {  if(err) {callback(false);}
+                                                            else    {var smtpTransport = nodemailer.createTransport('SMTP', config.mail);
+                                                                     confirmarEmailUrl += '/?account=' + doc._id;
+						                                             smtpTransport.sendMail({from: 'verao2015@ichose.com.br',
+                                                                                             to: doc.email,
+								                                                             subject: 'iChose - Verão 2015.',
+								                                                             text: 'Confirme aqui o recebimento do e-mail: ' + confirmarEmailUrl},function saveemailverao2015(err){ if(err) {callback(false);}
+								                         else    {callback(true);}});    
+                                                                }
+                                                     });    
 						}
 					}
 				});
