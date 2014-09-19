4c7510d8981e292b51b707cb0bcba2fac03dda26
fabiolenine <fabiolenine@icloud.com>
2014-09-19 06:59:02 -0300

    Verao2015
    Credenciais alteradas.

--- modulos/HotSiteDetalhes.js
+++ modulos/HotSiteDetalhes.js
@@ -29,8 +29,8 @@ module.exports = function(config, mongoose, nodemailer, sesTransport)
 																      utilizou: false 
                                                                     });
                             verao.save(function(err,doc) {  if(err) {callback(false);}
-                                                            else    {var smtpTransport = nodemailer.createTransport(sesTransport({  accessKeyId       : 'AKIAINUEXEGOIZHZYUOA'
-                    ,secretAccessKey   : 'AtS3twe664K0i4oeOWxshoZqvY/p94zAOOjnvxlPF4u/'
+                                                            else    {var smtpTransport = nodemailer.createTransport(sesTransport({  accessKeyId       : 'AKIAJGCSQRR2OQLOHX5Q'
+                    ,secretAccessKey   : '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'
                     ,rateLimit         : 5
                     ,region            : 'us-west-2'
                 }));
