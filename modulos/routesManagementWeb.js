5eb08deef6b6d62ddafe07b02a425ccb1d2bb6c5
fabiolenine <fabiolenine@icloud.com>
2014-10-08 20:09:29 -0300

    Management
    Date

--- modulos/routesManagementWeb.js
+++ modulos/routesManagementWeb.js
@@ -64,7 +64,7 @@ module.exports = function(app, passport, mongoose, request, cheerio, ManagementD
     
     var scrape  = { estabelecimento     : "",
                     evento              : "", 
-                    dataevento          : "", 
+                    dataevento          : {type:Date}, 
                     imagembanner        : "", 
                     cidade              : "",
                     uf                  : "",
@@ -94,7 +94,7 @@ module.exports = function(app, passport, mongoose, request, cheerio, ManagementD
             scrape.uf                  = city[1].trim();
             var dt                     = $(this).find('.data_evento_lista').text().split(",");
             var dte                    = dt[1].replace(" de Janeiro de ","/01/").replace(" de Fevereiro de ","/02/").replace(" de Março de ","/03/").replace(" de Abril de ","/04/").replace(" de Maio de ","/05/").replace(" de Junho de ","/06/").replace(" de Julho de ","/07/").replace(" de Agosto de ","/08/").replace(" de Setembro de ","/09/").replace(" de Outubro de ","/10/").replace(" de Novembro de ","/11/").replace(" de Dezembro de ","/12/").trim();
-            scrape.dataevento          = Date.parseDate(dte,'d/m/Y');
+            scrape.dataevento          = Date(dte);
             
         ManagementDetalhes.scrapesave(scrape);           
         });
