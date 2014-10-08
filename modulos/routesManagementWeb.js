904aa4b6a3fa10b4d3f3584d58a43685d7ffc49e
fabiolenine <fabiolenine@icloud.com>
2014-10-08 19:35:30 -0300

    Management
    Teste

--- modulos/routesManagementWeb.js
+++ modulos/routesManagementWeb.js
@@ -62,53 +62,45 @@ module.exports = function(app, passport, mongoose, request, cheerio, ManagementD
 // we will use route middleware to verify this (the isLoggedIn function)
   app.get('/scrape', function(req, res) {
     
-    function scrape(){
-        var scrape  = { estabelecimento     : "",
-                        evento              : "", 
-                        dataevento          : "", 
-                        imagembanner        : "", 
-                        cidade              : "",
-                        uf                  : "",
-                        abertura            : "", 
-                        inicio              : "", 
-                        classificacao       : "", 
-                        descricao           : "", 
-                        urlyoutube          : "",
-                        urlpersonaevento    : "",
-                        urlscrapedetalhes   : "",
-                        tags                : []};
-    };
-    
-    scrape();
+    var scrape  = { estabelecimento     : "",
+                    evento              : "", 
+                    dataevento          : "", 
+                    imagembanner        : "", 
+                    cidade              : "",
+                    uf                  : "",
+                    abertura            : "", 
+                    inicio              : "", 
+                    classificacao       : "", 
+                    descricao           : "", 
+                    urlyoutube          : "",
+                    urlpersonaevento    : "",
+                    urlscrapedetalhes   : "",
+                    tags                : []};
       
     ManagementDetalhes.scrapelink('http://www.blueticket.com.br/?secao=Eventos&tipo=6', function(html) {
         
         var $ = html;
         
-        scrape.tags.push($('.cabecalho .titulo').text().trim());
+        scrape.tags = $('.cabecalho .titulo').text().trim();
                 
-        var eventos = $('.item_evento_1');
-        
-        eventos.each(function(i,item){
-                    scrape.urlscrapedetalhes   = 'http://www.blueticket.com.br' +  $(item).find('a').attr('href').trim();
-                    scrape.imagembanner        = $(item).find('img').attr('src').trim();
-                    scrape.evento              = $(item).find('.titulo_evento_lista').text().trim();
-                    scrape.estabelecimento     = $(item).find('.desc_evento_lista strong').text().trim();
-                    var order                  = $(item).find('.desc_evento_lista').text().split("|");
+        $('.item_evento_1').each(function(){
+                    scrape.urlscrapedetalhes   = 'http://www.blueticket.com.br' +  $(this).find('a').attr('href').trim();
+                    scrape.imagembanner        = $(this).find('img').attr('src').trim();
+                    scrape.evento              = $(this).find('.titulo_evento_lista').text().trim();
+                    scrape.estabelecimento     = $(this).find('.desc_evento_lista strong').text().trim();
+                    var order                  = $(this).find('.desc_evento_lista').text().split("|");
                     var city                   = order[1].split("-");
                     scrape.cidade              = city[0].trim();
                     scrape.uf                  = city[1].trim();
-                    var dt                     = $(item).find('.data_evento_lista').text().split(",");
+                    var dt                     = $(this).find('.data_evento_lista').text().split(",");
                     scrape.dataevento          = dt[1].replace(" de Janeiro de ","/10/").replace(" de Fevereiro de ","/10/").replace(" de Março de ","/10/").replace(" de Abril de ","/10/").replace(" de Maio de ","/10/").replace(" de Junho de ","/10/").replace(" de Julho de ","/10/").replace(" de Agosto de ","/10/").replace(" de Setembro de ","/10/").replace(" de Outubro de ","/10/").replace(" de Novembro de ","/10/").replace(" de Dezembro de ","/10/").trim();
                     
         ManagementDetalhes.scrapesave(scrape, function(success) {
-            scrape();                
-        }); 
-                     
+            if(success){console.log(scrape);} else {console.log('error');}                
+        });           
         });
-    
-    res.send(200);
     });
+    res.send(200);
   });
     
 // =====================================
