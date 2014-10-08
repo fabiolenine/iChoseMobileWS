c8270a4047b0554347f515a4dae4f711d5902c55
fabiolenine <fabiolenine@icloud.com>
2014-10-08 05:53:48 -0300

    Management
    Scrape

--- modulos/ManagementDetalhes.js
+++ modulos/ManagementDetalhes.js
@@ -41,11 +41,10 @@ module.exports = function(mongoose, request, cheerio)
                     scrape.uf                  = city[1].trim();
                     var dt                     = $(this).find('.data_evento_lista').text().split(",");
                     scrape.dataevento          = dt[1].replace(" de Janeiro de ","/10/").replace(" de Fevereiro de ","/10/").replace(" de Março de ","/10/").replace(" de Abril de ","/10/").replace(" de Maio de ","/10/").replace(" de Junho de ","/10/").replace(" de Julho de ","/10/").replace(" de Agosto de ","/10/").replace(" de Setembro de ","/10/").replace(" de Outubro de ","/10/").replace(" de Novembro de ","/10/").replace(" de Dezembro de ","/10/").trim();
+                    scrapes.push(scrape);
                     counter--;
-                    if(counter==0){scrapes.push(scrape);}
-                });
-                
-                callback(scrapes);    
+                    if(counter==0){callback(scrapes);}
+                });    
             }
             else {
                 throw error;
