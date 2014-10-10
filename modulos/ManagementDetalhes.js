1d13b8dab663876bf1dead91ab77f7b560b55992
fabiolenine <fabiolenine@icloud.com>
2014-10-10 00:07:02 -0300

    Management
    teste

--- modulos/ManagementDetalhes.js
+++ modulos/ManagementDetalhes.js
@@ -31,7 +31,7 @@ module.exports = function(mongoose, request, cheerio)
         
             var $ = html;
         
-            eventscrape.classificacao = $('.desc_basica_evento p span strong img').text().trim();
+            eventscrape.classificacao = $('.desc_basica_evento p span img').text().trim();
             
             scrapesave(eventscrape);
             
