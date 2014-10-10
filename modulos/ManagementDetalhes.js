e9146693c88f20090d89df19e5c41011d1d74764
fabiolenine <fabiolenine@icloud.com>
2014-10-09 21:45:27 -0300

    Management
    Tentativa de loop no callback

--- modulos/ManagementDetalhes.js
+++ modulos/ManagementDetalhes.js
@@ -38,8 +38,10 @@ module.exports = function(mongoose, request, cheerio)
             var $ = html;
         
             scrape.tags = $('.cabecalho .titulo').text().trim();
-        
-            $('.item_evento_1').each(function(){
+            var events  = $('.item_evento_1')
+            var counter = events.length;
+            
+            events.each(function(){
                 scrape.urlscrapedetalhes   = 'http://www.blueticket.com.br' +  $(this).find('a').attr('href').trim();
                 scrape.imagembanner        = $(this).find('img').attr('src').trim();
                 scrape.evento              = $(this).find('.titulo_evento_lista').text().trim();
@@ -52,9 +54,10 @@ module.exports = function(mongoose, request, cheerio)
                 scrape.dataevento          = dt[1].replace(" de Janeiro de ","/01/").replace(" de Fevereiro de ","/02/").replace(" de Março de ","/03/").replace(" de Abril de ","/04/").replace(" de Maio de ","/05/").replace(" de Junho de ","/06/").replace(" de Julho de ","/07/").replace(" de Agosto de ","/08/").replace(" de Setembro de ","/09/").replace(" de Outubro de ","/10/").replace(" de Novembro de ","/11/").replace(" de Dezembro de ","/12/").trim();
             
                 scrapesave(scrape);
+                counter--;
+                if(counter==0){callback(true);}
             });
         });
-        callback(true);
     };
     
     var scrapeloaddetails = function(){
