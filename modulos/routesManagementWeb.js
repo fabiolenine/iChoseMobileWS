647e72470b0c3fb37b6d15938598c31094cfa0be
fabiolenine <fabiolenine@icloud.com>
2014-10-19 17:04:32 -0200

    Management
    router.

--- modulos/routesManagementWeb.js
+++ modulos/routesManagementWeb.js
@@ -132,7 +132,7 @@ module.exports = function(app, passport, mongoose, request, cheerio, ManagementD
 // we will want this protected so you have to be logged in to visit
 // we will use route middleware to verify this (the isLoggedIn function)
   app.get('/fornecedorcadastro', isLoggedIn, function(req, res) {
-    res.render('/fornecedorcadastro.ejs', {
+    res.render('fornecedorcadastro.ejs', {
       user : req.user // get the user out of session and pass to template
     });
   });
